df4c92203bf6291d893618456d4523f9d5a8f827
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-10-29 07:58:22 -0500

    env

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+ctiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
 require './app/controllers/application_controller'
 require_all 'app'
